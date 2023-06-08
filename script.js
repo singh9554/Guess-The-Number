be53dd700e8703ad5f6914f834b5a6e429910217
PankajSingh40 <14168421-PankajSingh40@users.noreply.replit.com>
2023-06-08 20:53:21 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+//Generate Number 1-100;
+document.querySelector(".Start").addEventListener("click", function() {
+  document.getElementsByClassName("Instruction")[0].innerHTML = "The Number has been generated";
+
+  let num = Math.floor(Math.random() * 100) + 1;
+  let noOfguess = 0;
+  let totalScore = 0;
+
+  document.querySelector(".btn").addEventListener("click", function() {
+    let guessNumber = parseInt(document.querySelector(".box").value);
+
+    if (guessNumber == num) {
+      document.getElementsByClassName("response")[0].innerHTML = "Congratulations! Your Guess is Correct.";
+      document.getElementsByClassName("guessNumber")[0].innerHTML = `Guessed Number is ${num}`;
+      totalScore = 100 - noOfguess;
+      document.getElementsByClassName("score")[0].innerHTML = `Your Final Score is ${totalScore}`;
+    } else if (guessNumber < num) {
+      document.getElementsByClassName("response")[0].innerHTML = "Your Guess is too low.";
+    } else {
+      document.getElementsByClassName("response")[0].innerHTML = "Your Guess is too high.";
+    }
+
+    noOfguess++;
+    document.getElementsByClassName("count")[0].innerHTML = `No of guess: ${noOfguess}`;
+  });
+});
